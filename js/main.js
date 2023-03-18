962389f2b7dd156cdcfa3dc9b200ac3824d2b5db
EuheniHreben <tomasblack3@gmail.com>
2023-03-18 10:22:51 +0300

    touch

--- js/main.js
+++ js/main.js
@@ -1,14 +1,43 @@
 const cols = document.querySelectorAll('.col');
+const screenWidth = window.screen.width;
 
-document.addEventListener('keydown', (event) => {
-  if (event.code.toLowerCase() === 'space') {
-    randomColor();
-  }
-})
+if (screenWidth<480) {
+  document.addEventListener('touchstart', (event) => {
+    const type = event.target.dataset.type;
+    if (type === 'lock') {
+      const node = event.target.tagName.toLowerCase() === 'i'
+      ? event.target
+      : event.target.children[0]
+      node.classList.toggle('fa-lock-open')
+      node.classList.toggle('fa-lock')
+    } else if (type === 'copy') {
+      clickCopy(event.target.textContent);
+    } else {
+      randomColor();
+    }
+  })
 
-document.addEventListener('touchstart', (event) => {
-  randomColor();
-})
+} else {
+  document.addEventListener('keydown', (event) => {
+    event.preventDefault();
+    if (event.code.toLowerCase() === 'space') {
+      randomColor();
+    }
+  })
+  
+  document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type;
+    if (type === 'lock') {
+      const node = event.target.tagName.toLowerCase() === 'i'
+      ? event.target
+      : event.target.children[0]
+      node.classList.toggle('fa-lock-open')
+      node.classList.toggle('fa-lock')
+    } else if (type === 'copy') {
+      clickCopy(event.target.textContent);
+    }
+  })
+}
 
 function randomGenerator() {
   const hexCodes = '0123456789ABCDEF';
@@ -24,6 +53,10 @@ function randomColor() {
     const text = col.querySelector('h2');
     const button = col.querySelector('button');
     const color = randomGenerator();
+    const isLocked = col.querySelector('i').classList.contains('fa-lock');
+    if (isLocked) {
+      return;
+    }
     text.textContent = color;
     col.style.background = randomGenerator();
     textColor(text, color);
@@ -34,6 +67,10 @@ function randomColor() {
 function textColor(text, color) {
   const luminance = chroma(color).luminance();
   text.style.color = luminance > .5 ? 'black' : 'white';
+}
+
+function clickCopy(text) {
+  return navigator.clipboard.writeText(text);
 }
 
 randomColor();
