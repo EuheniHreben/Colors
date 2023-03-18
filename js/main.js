d80f80cd17e409b45bb35d5488105a552806aa97
EuheniHreben <tomasblack3@gmail.com>
2023-03-18 06:11:38 +0300

    phone

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function randomGenerator() {
   const hexCodes = '0123456789ABCDEF';
   let color = '';
   for ( let i=0; i<6; i++) {
-    color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
   }
   return '#' + color;
 }
